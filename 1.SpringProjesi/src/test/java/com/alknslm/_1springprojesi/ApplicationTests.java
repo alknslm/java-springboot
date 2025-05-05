92b6c5ed70a16c2c42e6faeb08d858c007636757
alknslm <slmalkn.97@gmail.com>
2025-05-05 16:26:08 +0300

    spring-başlangıç

---
+++ 1.SpringProjesi/src/test/java/com/alknslm/_1springprojesi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alknslm._1springprojesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
