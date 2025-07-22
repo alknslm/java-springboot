585b9eb6ce54610fa2ca96dcc3c439e08f70a45f
alknslm <slmalkn.97@gmail.com>
2025-07-22 18:00:22 +0300

    spring'te JUnit test nasıl yazılır

--- exception-management/src/main/java/com/alknslm/scheduled/ScheduledExample.java
+++ exception-management/src/main/java/com/alknslm/scheduled/ScheduledExample.java
@@ -6,17 +6,17 @@ import org.springframework.stereotype.Component;
 @Component //spring container'da nesnesi üeritlsin diye konuldu
 public class ScheduledExample {
 
-    @Scheduled(cron = "0 30 17 * * *") // saat 17.27'de servisi çalıştır.
-    public void write1To10(){
-        for(int i=1; i<=10; i++){
-            System.out.print(i+ " ");
-        }
-    }
-
-    @Scheduled(cron = "*/5 * * * * *") // her 5 snde çalış.
-    public void writeEvery5second(){
-        for(int i=1; i<=10; i++){
-            System.out.print(i+ " ");
-        }
-    }
+//    @Scheduled(cron = "0 30 17 * * *") // saat 17.27'de servisi çalıştır.
+//    public void write1To10(){
+//        for(int i=1; i<=10; i++){
+//            System.out.print(i+ " ");
+//        }
+//    }
+//
+//    @Scheduled(cron = "*/5 * * * * *") // her 5 snde çalış.
+//    public void writeEvery5second(){
+//        for(int i=1; i<=10; i++){
+//            System.out.print(i+ " ");
+//        }
+//    }
 }
--- exception-management/src/test/java/com/alknslm/starter/ExceptionManagementApplicationTests.java
+++ exception-management/src/test/java/com/alknslm/starter/ExceptionManagementApplicationTests.java
@@ -1,13 +1,57 @@
 package com.alknslm.starter;
 
+import com.alknslm.dto.DtoEmployee;
+import com.alknslm.service.IEmployeeService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(classes = {ExceptionManagementStarter.class}) //test sınıfı olduğunu belirtmek için
 class ExceptionManagementApplicationTests {
 
-    @Test
-    void contextLoads() {
+    /** ---- TESTLER CONTROLLER KATMANI İLE DEĞİL SERVICE KATMANI İLE YAZILIR ----*/
+
+    @Autowired
+    private IEmployeeService employeeService;
+
+    /**
+     * Asıl test method'dan önce veritabanı bağlantısı için bir test methodu yazılabilir.
+     * Asıl test edilecek method için bir token'a ihtiyaç vardır öncesinde o token'ı alıp burada oluşturabilriiz
+     */
+    @BeforeEach //her test sınıfında önce çalışacak method
+    public void beforeEach(){
+        System.out.println("Before Each Çalıştı");
+    }
+
+    /**
+     * test+(TEST EDİLECEK METHOD) olarak isimlendirmek doğru olan
+     */
+    @Test //test methodu olduğunu göstermek için
+    public void testFindEmployeeById(){
+        DtoEmployee employee = employeeService.findEmployeeById(3L);
+
+        assertEquals("Ahmet Işık",employee.getName()); //servisten beklenen ve dönen değer birbirine eişt mi
+
+//        assertNotNull(employee); // null'sa hata verir
+
+
+//        if(employee!=null){
+//            System.out.println("İsim : " + employee.getName());
+//        }
+    }
+
+    /**
+     * Her method çalıştıktan sonra email at
+     * Asıl test method'u çalıştıktan sonra burayı temizle, buraya yaz vb.
+     */
+    @AfterEach // her test sınıfından sonra çalışacak method
+    public void afterEach(){
+        System.out.println("After Each Çalıştı");
     }
 
 }
