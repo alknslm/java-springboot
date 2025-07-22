4b30a15499e2b24c5da52f503ed23fb763fb95fa
alknslm <slmalkn.97@gmail.com>
2025-07-22 17:33:04 +0300

    spring'te schedule anotasyonu kullanımı(zamanlayıcı)

---
+++ exception-management/src/main/java/com/alknslm/scheduled/ScheduledExample.java
@@ -0,0 +1,22 @@
+package com.alknslm.scheduled;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component //spring container'da nesnesi üeritlsin diye konuldu
+public class ScheduledExample {
+
+    @Scheduled(cron = "0 30 17 * * *") // saat 17.27'de servisi çalıştır.
+    public void write1To10(){
+        for(int i=1; i<=10; i++){
+            System.out.print(i+ " ");
+        }
+    }
+
+    @Scheduled(cron = "*/5 * * * * *") // her 5 snde çalış.
+    public void writeEvery5second(){
+        for(int i=1; i<=10; i++){
+            System.out.print(i+ " ");
+        }
+    }
+}
--- exception-management/src/main/java/com/alknslm/starter/ExceptionManagementStarter.java
+++ exception-management/src/main/java/com/alknslm/starter/ExceptionManagementStarter.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
 
 @EntityScan(basePackages = {"com.alknslm"})
 @EnableJpaRepositories(basePackages = {"com.alknslm"})
 @ComponentScan(basePackages = {"com.alknslm"})
+@EnableScheduling // schedule ile çalışacak şeyleri aktif hale getirmek için
 @SpringBootApplication
 public class ExceptionManagementStarter {
 
